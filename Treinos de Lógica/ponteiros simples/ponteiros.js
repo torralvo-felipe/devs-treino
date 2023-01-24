e560b83b969f5c4e180dfed57402ff26b69fd5e5
Felipe André Torralvo <123479952+torralvo-felipe@users.noreply.github.com>
2023-01-24 20:13:26 -0300

    Ponteiros simples

---
+++ Treinos de Lógica/ponteiros simples/ponteiros.js
@@ -0,0 +1,15 @@
+var arr = [1, 55, 2, 110, 3, 165, 4, 220, 5, 275]
+
+var inverter = function () {
+    var mid = (arr.length/2)
+    for (i = 0; i < mid; i++){
+        numSave = arr[i]
+        arr[i] = arr[(arr.length-1)-i]
+        arr[(arr.length-1)-i] = numSave
+    }
+    
+    arr.forEach((i, idx) => {
+        if (idx < arr.length-1) document.getElementById('reverse').innerHTML += i+', '
+        else document.getElementById('reverse').innerHTML += i
+    })
+}
